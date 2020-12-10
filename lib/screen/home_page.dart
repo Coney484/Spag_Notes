92e9e7d710d1719a29e267b21b56cebcc9c617bb
ROHAN CHOUDHARY <anandc.rohan484@gmail.com>
2020-12-10 10:05:19 +0530

    home_screen ui and functionality

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,143 +1,294 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:spag_notes/model/filter.dart';
 import 'package:spag_notes/model/note.dart';
 import 'package:spag_notes/model/user.dart';
+import 'package:spag_notes/service/note_services.dart';
+import 'package:spag_notes/widget/drawer.dart';
 import 'package:spag_notes/widget/notes_grid.dart';
 import 'package:spag_notes/widget/notes_list.dart';
+import 'package:tuple/tuple.dart';
 
-class HomePage extends StatefulWidget {
+import '../styles.dart';
+import '../utils.dart';
+
+
+
+/// Home screen, displays [Note] grid or list.
+class HomeScreen extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  State<StatefulWidget> createState() => _HomeScreenState();
 }
 
-class _HomePageState extends State<HomePage> {
+/// [State] of [HomeScreen].
+class _HomeScreenState extends State<HomeScreen> with CommandHandler {
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  /// `true` to show notes in a GridView, a ListView otherwise.
   bool _gridView = true;
 
   @override
-  Widget build(BuildContext context) => StreamProvider.value(
-        value: _createNoteStream(context),
-        child: Scaffold(
-          body: CustomScrollView(
-            slivers: <Widget>[
-              _appBar(context),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 24,
-                ),
-              ),
-              _buildNotesView(context),
-              const SliverToBoxAdapter(
-                child: SizedBox(
-                  height: 80.0,
-                ),
-              ),
-            ],
-          ),
-          floatingActionButton: _fab(context),
-          bottomNavigationBar: _bottomActions(),
-          floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
-          extendBody: true,
+  Widget build(BuildContext context) => AnnotatedRegion<SystemUiOverlayStyle>(
+    value: SystemUiOverlayStyle.dark.copyWith(
+//      statusBarColor: Colors.white,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ),
+    child: MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => NoteFilter(), // watching the note filter
         ),
-      );
-
-
-  Widget _appBar(BuildContext context) => SliverAppBar(
-    floating: true,
-    snap: true,
-    title: _topActions(context),
-    automaticallyImplyLeading: false,
-    centerTitle: true,
-    titleSpacing: 0,
-    backgroundColor: Colors.transparent,
-    elevation: 0,
+        Consumer<NoteFilter>(
+          builder: (context, filter, child) => StreamProvider.value(
+            value: _createNoteStream(context, filter), // applying the filter to Firestore query
+            child: child,
+          ),
+        ),
+      ],
+      child: Consumer2<NoteFilter, List<Note>>(
+        builder: (context, filter, notes, child) {
+          final hasNotes = notes?.isNotEmpty == true;
+          final canCreate = filter.noteState.canCreate;
+          return Scaffold(
+            key: _scaffoldKey,
+            body: Center(
+              child: ConstrainedBox(
+                constraints: const BoxConstraints.tightFor(width: 720),
+                child: CustomScrollView(
+                  slivers: <Widget>[
+                    _appBar(context, filter, child),
+                    if (hasNotes) const SliverToBoxAdapter(
+                      child: SizedBox(height: 24),
+                    ),
+                    ..._buildNotesView(context, filter, notes),
+                    if (hasNotes) SliverToBoxAdapter(
+                      child: SizedBox(height: (canCreate ? kBottomBarSize : 10.0) + 10.0),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            drawer: AppDrawer(),
+            floatingActionButton: canCreate ? _fab(context) : null,
+            bottomNavigationBar: canCreate ? _bottomActions() : null,
+            floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+            extendBody: true,
+          );
+        },
+      ),
+    ),
   );
 
-  Widget _topActions(BuildContext context) => Container(
-        width: double.infinity,
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: Card(
-          elevation: 2.0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 5.0),
-            child: Row(
-              children: <Widget>[
-                const SizedBox(width: 20.0),
-                Icon(Icons.menu),
-                Expanded(
-                  child: Text('Search your notes', softWrap: false),
-                ),
-                InkWell(
-                  child: Icon(_gridView ? Icons.view_list : Icons.view_module),
-                  onTap: () => setState(() {
-                    _gridView = !_gridView;
-                  }),
-                ),
-                SizedBox(
-                  width: 18,
-                ),
-                _buildAvatar(context),
-                SizedBox(
-                  width: 10.0,
-                ),
-              ],
-            ),
-          ),
+  Widget _appBar(BuildContext context, NoteFilter filter, Widget bottom) =>
+    filter.noteState < NoteState.archived
+      ? SliverAppBar(
+        floating: true,
+        snap: true,
+        title: _topActions(context),
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        titleSpacing: 0,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      )
+      : SliverAppBar(
+        floating: true,
+        snap: true,
+        title: Text(filter.noteState.filterName),
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          tooltip: 'Menu',
+          onPressed: () => _scaffoldKey.currentState?.openDrawer(),
         ),
+        automaticallyImplyLeading: false,
       );
+
+  Widget _topActions(BuildContext context) => Container(
+    // width: double.infinity,
+    constraints: const BoxConstraints(
+      maxWidth: 720,
+    ),
+    padding: const EdgeInsets.symmetric(horizontal: 20),
+    child: Card(
+      elevation: 2,
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: isNotAndroid ? 7 : 5),
+        child: Row(
+          children: <Widget>[
+            const SizedBox(width: 20),
+            InkWell(
+              child: const Icon(Icons.menu),
+              onTap: () => _scaffoldKey.currentState?.openDrawer(),
+            ),
+            const SizedBox(width: 16),
+            const Expanded(
+              child: Text('Search your notes',
+                softWrap: false,
+                style: TextStyle(
+                  color: kHintTextColorLight,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+            const SizedBox(width: 16),
+            InkWell(
+              child: Icon(_gridView ? Icons.view_list : Icons.grid_on),
+              onTap: () => setState(() {
+                _gridView = !_gridView;
+              }),
+            ),
+            const SizedBox(width: 18),
+            _buildAvatar(context),
+            const SizedBox(width: 10),
+          ],
+        ),
+      ),
+    ),
+  );
 
   Widget _bottomActions() => BottomAppBar(
-        shape: CircularNotchedRectangle(),
-        child: Container(
-          height: kBottomNavigationBarHeight,
-          padding: EdgeInsets.symmetric(horizontal: 17),
-          child: Row(),
-        ),
-      );
+    shape: const CircularNotchedRectangle(),
+    child: Container(
+      height: kBottomBarSize,
+      padding: const EdgeInsets.symmetric(horizontal: 17),
+      child: Row(
+        children: <Widget>[
+          const Icon(Icons.check_box, size: 26, color: kIconTintLight),
+          const SizedBox(width: 30),
+          const Icon(Icons.brush_sharp, size: 26, color: kIconTintLight),
+          const SizedBox(width: 30),
+          const Icon(Icons.mic, size: 26, color: kIconTintLight),
+          const SizedBox(width: 30),
+          const Icon(Icons.insert_photo, size: 26, color: kIconTintLight),
+        ],
+      ),
+    ),
+  );
 
   Widget _fab(BuildContext context) => FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
-      );
+    backgroundColor: Theme.of(context).accentColor,
+    child: const Icon(Icons.add),
+    onPressed: () async {
+      final command = await Navigator.pushNamed(context, '/note');
+      debugPrint('--- noteEditor result: $command');
+      processNoteCommand(_scaffoldKey.currentState, command);
+    },
+  );
 
   Widget _buildAvatar(BuildContext context) {
     final url = Provider.of<CurrentUser>(context)?.data?.photoUrl;
     return CircleAvatar(
       backgroundImage: url != null ? NetworkImage(url) : null,
-      child: url == null ? Icon(Icons.face) : null,
-      radius: 17,
+      child: url == null ? const Icon(Icons.face) : null,
+      radius: isNotAndroid ? 19 : 17,
     );
   }
 
-  Widget _buildNotesView(BuildContext context) => Consumer<List<Note>>(
-        builder: (context, notes, _) {
-          if (notes?.isNotEmpty != true) {
-            return _buildBlankView();
-          }
+  /// A grid/list view to display notes
+  ///
+  /// Notes are divided to `Pinned` and `Others` when there's no filter,
+  /// and a blank view will be rendered, if no note found.
+  List<Widget> _buildNotesView(BuildContext context, NoteFilter filter, List<Note> notes) {
+    if (notes?.isNotEmpty != true) {
+      return [_buildBlankView(filter.noteState)];
+    }
 
-          final widget = _gridView ? NotesGrid.create : NotesList.create;
-          return widget(notes: notes, onTap: (_) {});
-        },
-      );
+    final asGrid = filter.noteState == NoteState.deleted || _gridView;
+    final factory = asGrid ? NotesGrid.create : NotesList.create;
+    final showPinned = filter.noteState == NoteState.unspecified;
 
-  Widget _buildBlankView() => SliverFillRemaining(
-        hasScrollBody: false,
-        child: Text(
-          'Notes you add appear here',
-          style: TextStyle(
-            color: Colors.black54,
-            fontSize: 14,
+    if (!showPinned) {
+      return [
+        factory(notes: notes, onTap: _onNoteTap),
+      ];
+    }
+
+    final partition = _partitionNotes(notes);
+    final hasPinned = partition.item1.isNotEmpty;
+    final hasUnpinned = partition.item2.isNotEmpty;
+
+    final _buildLabel = (String label, [double top = 26]) => SliverToBoxAdapter(
+      child: Container(
+        padding: EdgeInsetsDirectional.only(start: 26, bottom: 25, top: top),
+        child: Text(label, style: const TextStyle(
+          color: kHintTextColorLight,
+          fontWeight: FontWeights.medium,
+          fontSize: 12),
+        ),
+      ),
+    );
+
+    return [
+      if (hasPinned) _buildLabel('PINNED', 0),
+      if (hasPinned) factory(notes: partition.item1, onTap: _onNoteTap),
+      if (hasPinned && hasUnpinned) _buildLabel('OTHERS'),
+      factory(notes: partition.item2, onTap: _onNoteTap),
+    ];
+  }
+
+  Widget _buildBlankView(NoteState filteredState) => SliverFillRemaining(
+    hasScrollBody: false,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const Expanded(flex: 1, child: SizedBox()),
+        Icon(Icons.note_outlined,
+          size: 120,
+          color: kAccentColorLight.shade300,
+        ),
+        Expanded(
+          flex: 2,
+          child: Text(filteredState.emptyResultMessage,
+            style: TextStyle(
+              color: kHintTextColorLight,
+              fontSize: 14,
+            ),
           ),
         ),
-      );
+      ],
+    ),
+  );
 
-  Stream<List<Note>> _createNoteStream(BuildContext context) {
-    final uid = Provider.of<CurrentUser>(context)?.data?.uid;
-    return Firestore.instance
-        .collection('notes-$uid')
-        .where('state', isEqualTo: 0)
-        .snapshots()
-        .handleError((e) => debugPrint('query notes failed: $e'))
-        .map((snapshot) => Note.fromQuery(snapshot));
+  /// Callback on a single note clicked
+  void _onNoteTap(Note note) async {
+    final command = await Navigator.pushNamed(context, '/note', arguments: { 'note': note });
+    processNoteCommand(_scaffoldKey.currentState, command);
+  }
+
+  /// Create notes query
+  Stream<List<Note>> _createNoteStream(BuildContext context, NoteFilter filter) {
+    final user = Provider.of<CurrentUser>(context)?.data;
+    final sinceSignUp = DateTime.now().millisecondsSinceEpoch -
+      (user?.metadata?.creationTime?.millisecondsSinceEpoch ?? 0);
+    final useIndexes = sinceSignUp >= _10_min_millis; // since creating indexes takes time, avoid using composite index until later
+    final collection = notesCollection(user?.uid);
+    final query = filter.noteState == NoteState.unspecified
+      ? collection
+        .where('state', isLessThan: NoteState.archived.index) // show both normal/pinned notes when no filter specified
+        .orderBy('state', descending: true) // pinned notes come first
+      : collection.where('state', isEqualTo: filter.noteState.index);
+
+    return (useIndexes ? query.orderBy('createdAt', descending: true) : query)
+      .snapshots()
+      .handleError((e) => debugPrint('query notes failed: $e'))
+      .map((snapshot) => Note.fromQuery(snapshot));
+  }
+
+  /// Partition the note list by the pinned state
+  Tuple2<List<Note>, List<Note>> _partitionNotes(List<Note> notes) {
+    if (notes?.isNotEmpty != true) {
+      return Tuple2([], []);
+    }
+
+    final indexUnpinned = notes?.indexWhere((n) => !n.pinned);
+    return indexUnpinned > -1
+      ? Tuple2(notes.sublist(0, indexUnpinned), notes.sublist(indexUnpinned))
+      : Tuple2(notes, []);
   }
 }
+
+const _10_min_millis = 600000;
